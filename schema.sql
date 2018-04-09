5030d006c6a4f783291eb37c7597675eb52a9277
John De Jesus <Jd@Johns-MacBook-Pro.local>
2018-04-08 17:02:38 -0700

    added

---
+++ schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon
+
+CREATE DATABASE bamazon
+
+USE bamazon
+
+CREATE TABLE products (
+    id VARCHAR(11) AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+    product_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    price INTEGER(11) NOT NULL,
+    stock_quantity INTEGER(11) NOT NULL,
+)
